8cc6b8b3436e9a12678f024df85bc9cff1431627
tejaskapse1902 <tejaskapse1902@gmail.com>
2026-01-10 13:56:52 +0530

    Cors Error Fix

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.api_router import api_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",   # React (Vite)
+        "http://localhost:3000",   # React (CRA)
+        "https://your-frontend-domain.com"  # later
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(api_router)
 
